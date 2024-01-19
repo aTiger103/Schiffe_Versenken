c1f74d9624804021ce384891932ca98fce11ab54
aTiger103 <136513536+aTiger103@users.noreply.github.com>
2024-01-19 11:34:48 +0100

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -10,11 +10,16 @@
 
 
 int main()
-{
-    Menu menu;
+{   // Spieler anlegen
     auto ki = std::make_shared<Spieler>(SpielerTyp::KI);
     auto spieler = std::make_shared<Spieler>(SpielerTyp::Spieler);
+
+    // um das Spiel zu initialisieren, wird ein Menü angelegt
+    Menu menu;
+    // hier wird das Menü für den menschlichen Spieler angezeigt 
+    // und somit kann der Spieler eine der möglichen Optionen auswählen, um das Spiel beispielsweise zu starten  
     menu.spielMenu(spieler, ki);
+
 
     return 0;
 }
